d924c5b96c1d8a3837e898c7646c0ed4f9543ab4
Soupinski <sssoupinski@gmail.com>
2023-05-04 19:40:40 -0400

    feat: chart generator for answer rate.

---
+++ src/components/ChartGenerator.vue
@@ -0,0 +1,43 @@
+<script>
+  import Chart from 'chart.js'
+
+  export default {
+    props: {
+      chartData: {
+        type: Array,
+        required: true
+      }
+    },
+    mounted() {
+      const chartData = this.chartData
+      const ctx = this.$refs.myChart.getContext('2d')
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: chartData.map(data => data.month),
+          datasets: [{
+            label: 'Answer Rate',
+            data: chartData.map(data => data.answer_rate),
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      }
+    )
+  },
+}
+</script>
+
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
--- src/components/ProjectInfo.vue
+++ src/components/ProjectInfo.vue
@@ -1,85 +1,14 @@
 <script>
-  import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
-
-  import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    LineElement,
-    LinearScale,
-    CategoryScale,
-    PointElement
-  } from 'chart.js'
-
-  ChartJS.register(
-    Title,
-    Tooltip,
-    Legend,
-    LineElement,
-    LinearScale,
-    CategoryScale,
-    PointElement
-  )
+  import axios from "axios"
+  import ChartGenerator from './ChartGenerator.vue'
 
   export default {
     name: 'ProjectInfo',
     components: {
-      LineChartGenerator
-    },
-    props: {
-      chartId: {
-        type: String,
-        default: 'line-chart'
-      },
-      datasetIdKey: {
-        type: String,
-        default: 'label'
-      },
-      width: {
-        type: Number,
-        default: 400
-      },
-      height: {
-        type: Number,
-        default: 400
-      },
-      cssClasses: {
-        default: '',
-        type: String
-      },
-      styles: {
-        type: Object,
-        default: () => {}
-      },
-      plugins: {
-        type: Array,
-        default: () => []
-      }
+      ChartGenerator
     },
     data: () => ({
-      chartData: {
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
-        ],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40]
-          }
-        ]
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
+      answerRates: null
     }),
 
     computed: {
@@ -87,6 +16,15 @@
         const projectName = this.$route.path.split('/')[2] || ''
         return projectName[0].toUpperCase() + projectName.slice(1)
       }
+    },
+
+    async mounted() {
+      const projectTag = this.$route.path.split('/')[2] || ''
+      const apiResponse = await axios.get(
+        `${process.env.VUE_APP_API_HOST}/answer-rate/${projectTag}`
+      )
+      this.answerRates = apiResponse.data.answer_rates
+      console.log('this.answerRates', this.answerRates)
     },
 
     methods: {
@@ -101,17 +39,7 @@
       <h1>{{ getProjectName }}</h1>
     </div>
     <div class="mt-15" data-cy="project-info-chart">
-      <LineChartGenerator
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
+      <chart-generator :chart-data="answerRates"></chart-generator>
     </div>
 
   </v-container>
